30e731bd8d868ae90213ea2e3504593772390774
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-21 15:18:21 -0300

    :sparkles: feat(src): import header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import PreviousEvents from "./routes/PreviousEvents";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
