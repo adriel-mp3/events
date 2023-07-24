d2714ad7e4bb62cb7934500883c4aaeebf394687
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-24 14:06:49 -0300

    :lipstick: feat(src/routes) add home components

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import FeaturedContent from "../components/FeaturedContent/FeaturedContent";
+import Thumb from "../components/Thumb/Thumb";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="grid grid-cols-2 max-w-[1600px] mx-auto justify-items-start items-center">
+      <FeaturedContent />
+      <Thumb/>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
