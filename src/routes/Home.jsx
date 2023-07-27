2d356d8191497a87d2174dd4d0d86343f0280815
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-27 15:29:33 -0300

    :sparkles: feat(routes): add home route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,7 +4,7 @@ import Thumb from "../components/Thumb/Thumb";
 
 const Home = () => {
   return (
-    <main className="grid grid-cols-2 max-w-[1600px] mx-auto justify-items-start items-center">
+    <main className="grid lg:grid-cols-2 md:grid-cols-1 max-w-[1600px] mx-auto justify-items-start px-10 gap-20 xl:mb-0 sm:mb-8">
       <FeaturedContent />
       <Thumb/>
     </main>
