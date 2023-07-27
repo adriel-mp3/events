0e32c54c50a0de2419f5d04ffff1355cb5f87ed3
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-27 15:34:32 -0300

    :sparkles: feat(components): create thumb component

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import thumb from "../../assets/images/thumb.jpg";
 const Thumb = () => {
   return (
-    <div className="relative">
-      <div className="after:content-[''] after:bg-play after:w-[55px] after:h-[66px] after:absolute flex justify-center items-center">
-        <img src={thumb} alt="Thumbnail video" />
-        </div>
+    <div className="relative after:content-[''] after:bg-play after:w-[55px] after:h-[66px] after:absolute flex items-center justify-center lg:mb-0 mb-10">
+      <img
+        src={thumb}
+        alt="Thumbnail video"
+      />
     </div>
   );
 };
