54d64eb502beb8fd66194ead2ae83199e0b5d642
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-27 15:29:23 -0300

    :sparkles: feat(routes): add about route

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import FeaturedContent from "../components/FeaturedContent/FeaturedContent";
+import AboutContent from "../components/AboutContent/AboutContent";
+import useMedia from '../hooks/useMedia'
 const About = () => {
+  const screen = useMedia('(min-width:1535px)') 
+  
   return (
-    <div>About</div>
-  )
-}
+    <main className="grid 2xl:grid-cols-2 max-w-[1600px] mx-auto 2xl:justify-items-end items-center px-10">
+      <AboutContent />
+      {screen && <FeaturedContent />}
+    </main>
+  );
+};
 
-export default About
+export default About;
