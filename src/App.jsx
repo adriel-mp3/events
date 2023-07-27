c445e9bd77cc4b5457f2351d6374eedc03a7f307
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-27 15:28:56 -0300

    :sparkles: feat(src): add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import PreviousEvents from "./routes/PreviousEvents";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/previous-events" element={<PreviousEvents />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="flex flex-col h-screen">
+      <BrowserRouter>
+        <Header />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/previous-events" element={<PreviousEvents />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
