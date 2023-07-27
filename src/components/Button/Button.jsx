cf5539160395aa78f4f92dce4901b12bc0b8cc67
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-27 15:32:37 -0300

    :sparkles: feat(components): create button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ children, style, onClick }) => {
   return (
-    <button>Button</button>
-  )
-}
+    <button
+      className={`text-center text-white py-2 px-6 text-white-100 font-medium ${style}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
