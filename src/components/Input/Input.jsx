5f687e2d396f3e95649e65d6685bf563707a0301
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-27 15:33:40 -0300

    :sparkles: feat(components): create input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Input = ({ placeholder, label, id, type }) => {
+  return (
+    <>
+      <label className={` ${label ? "text-gray" : "sr-only"}`}>
+        {label}
+      </label>
+      <input
+        type={type ? type : 'type'}
+        id={id ? id : "input"}
+        className="bg-white-60 text-gray px-4 py-2 block"
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default Input;
