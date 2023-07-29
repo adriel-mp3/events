11955605aa806da1ae17a96e536b4cba7a928ae1
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-29 14:34:17 -0300

    :recycle: refactor: replace default font-size and font-weight tailwind classes with project design system

--- src/components/AboutContent/AboutItem.jsx
+++ src/components/AboutContent/AboutItem.jsx
@@ -5,7 +5,7 @@ const AboutItem = ({ img, title, text, style, alt }) => {
     <li className={`bg-purple flex flex-col items-center ${style}`}>
       <img className="mb-1" src={img} alt={alt} />
       <h2 className="text-yellow mb-2 text-2xl font-medium">{title}</h2>
-      <p className="text-white-100">{text}</p>
+      <p className="text-white-100 text-base text-center">{text}</p>
     </li>
   );
 };
