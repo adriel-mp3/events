8e79b0cf7b58d90c6a9d3b596653b6e847affe7d
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-30 16:40:03 -0300

    :recycle: refactor: add container class

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,7 +4,7 @@ import Thumb from "../components/Thumb/Thumb";
 
 const Home = () => {
   return (
-    <main className="grid lg:grid-cols-2 md:grid-cols-1 max-w-[1600px] mx-auto justify-items-start px-10 gap-20 xl:mb-0 sm:mb-8">
+    <main className="container grid lg:grid-cols-2 md:grid-cols-1 justify-items-start gap-20 xl:mb-0 sm:mb-8">
       <FeaturedContent />
       <Thumb />
     </main>
--- src/routes/PreviousEvents.jsx
+++ src/routes/PreviousEvents.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PreviousContent from "../components/PreviousContent/PreviousContent";
 const PreviousEvents = () => {
   return (
-    <main className="max-w-[1600px] mx-auto px-6 ">
+    <main className="container">
       <PreviousContent />
     </main>
   );
