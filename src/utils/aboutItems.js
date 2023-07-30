d80a6e56fa7b5ab896e1b0d2c7bac1013e9700a1
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-30 17:36:30 -0300

    :sparkles: feat(utils): add utils

---
+++ src/utils/aboutItems.js
@@ -0,0 +1,29 @@
+import hobbies from "../assets/images/hobbies.svg";
+import location from "../assets/images/location.svg";
+import presentation from "../assets/images/presentation.svg";
+
+const aboutData = [
+  {
+    style: "2xl:px-[2.5rem] 2xl:py-[4.38rem] 2xl:my-8",
+    img: location,
+    alt: "Location icon",
+    title: "Location",
+    text: "Welcome to Villa Lorem, the perfect place to host memorable events! Situated in a picturesque setting surrounded by lush nature, Villa Lorem is the ideal destination.",
+  },
+  {
+    style: "2xl:px-[2.5rem] 2xl:py-[5.12rem] 2xl:my-4",
+    img: hobbies,
+    alt: "Hobbies icon",
+    title: "Hobbies",
+    text: "Join our fantastic event and explore exciting hobbies! From thrilling outdoor adventures to creative workshops, there's something for everyone!",
+  },
+  {
+    style: "2xl:px-[2.5rem] 2xl:py-[5.81rem]",
+    img: presentation,
+    alt: "Presentation icon",
+    title: "Presentation",
+    text: "Spectacular event! Get ready to be amazed by breathtaking performances, captivating artists, and a night filled with magic and wonder.",
+  },
+];
+
+export default aboutData;
---
+++ src/utils/headerNavLinks.js
@@ -0,0 +1,17 @@
+const navLinksData = [
+  {
+    to: "/",
+    end: true,
+    label: "Home",
+  },
+  {
+    to: "/about",
+    label: "About",
+  },
+  {
+    to: "/previous-events",
+    label: "Previous Events",
+  },
+];
+
+export default navLinksData;
