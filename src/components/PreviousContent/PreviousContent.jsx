6532c89ae44034fa7761c509085f25d27a8519ff
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-30 16:16:53 -0300

    :sparkles: feat(components): import previousItemsData for PreviousContent component

--- src/components/PreviousContent/PreviousContent.jsx
+++ src/components/PreviousContent/PreviousContent.jsx
@@ -1,37 +1,19 @@
 import React from "react";
 import PreviousItem from "./PreviousItem";
-import networking from "../../assets/images/networking.jpg";
-import product from "../../assets/images/product.jpg";
-import science from "../../assets/images/science.jpg";
-import hard from "../../assets/images/hard.jpg";
+import previousItemsData from "../../helper/previousItems";
 
 const PreviousContent = () => {
   return (
     <section className="flex justify-between gap-8 overflow-x-auto mb-10">
-      <PreviousItem
-        label="2019"
-        event="Networking Project"
-        img={networking}
-        alt="Networking event photo"
-      />
-      <PreviousItem
-        label="2020"
-        event="Science Hub | WEF"
-        img={product}
-        alt="Science Hu event photo"
-      />
-      <PreviousItem
-        label="2021"
-        event="#ProductCon | London"
-        img={science}
-        alt="#ProductCon event photo"
-      />
-      <PreviousItem
-        label="2022"
-        event="#HardCoreCoding"
-        img={hard}
-        alt="#HardCoreCoding event photo"
-      />
+      {previousItemsData.map((item) => (
+        <PreviousItem
+          key={item.event}
+          label={item.label}
+          event={item.event}
+          img={item.img}
+          alt={item.alt}
+        />
+      ))}
     </section>
   );
 };
