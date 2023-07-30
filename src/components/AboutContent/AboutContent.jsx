232bc12f9da1da0713300fd9788113c41a8c2220
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-30 16:20:53 -0300

    :sparkles: feat(components): import aboutData helper

--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -1,40 +1,22 @@
 import React from "react";
-import location from "../../assets/images/location.svg";
-import hobbies from "../../assets/images/hobbies.svg";
-import presentation from "../../assets/images/presentation.svg";
+import aboutData from "../../helper/aboutItems";
 import AboutItem from "./AboutItem";
 
 const AboutContent = () => {
   return (
     <section className="overflow-x-auto ">
       <ul className="flex content-center 2xl:mb-0 mb-10 2xl:gap-0 gap-8 ">
-        <AboutItem
-          style="2xl:px-[2.5rem] 2xl:py-[4.38rem] 2xl:my-8 px-8 py-[5rem] min-w-[280px]"
-          img={location}
-          alt="Location icon"
-          title="Location"
-          text="Welcome to Villa Lorem, the perfect place to host memorable events!
-            Situated in a picturesque setting surrounded by lush nature, Villa
-            Lorem is the ideal destination."
-        />
-        <AboutItem
-          style="2xl:px-[2.5rem] 2xl:py-[5.12rem] 2xl:my-4 px-8 py-[5rem] min-w-[280px]"
-          img={hobbies}
-          alt="Hobbies icon"
-          title="Hobbies"
-          text="Join our fantastic event and explore exciting hobbies! From
-          thrilling outdoor adventures to creative workshops, there's
-          something for everyone!"
-        />
-        <AboutItem
-          style="2xl:px-[2.5rem] 2xl:py-[5.81rem] px-8 py-[5rem] min-w-[280px]"
-          img={presentation}
-          alt="Presentation icon"
-          title="Presentation"
-          text="Spectacular event! Get ready to be amazed by breathtaking
-          performances, captivating artists, and a night filled with magic and
-          wonder."
-        />
+        {aboutData.map((card) => (
+          <div key={card.title}>
+            <AboutItem
+              img={card.img}
+              alt={card.alt}
+              title={card.title}
+              text={card.text}
+              style={card.style}
+            />
+          </div>
+        ))}
       </ul>
     </section>
   );
