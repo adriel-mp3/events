938690128e6fc63ea96aeb3920255971985d90f6
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-30 16:21:52 -0300

    :lipstick: feat: refactor about item styles

--- src/components/AboutContent/AboutItem.jsx
+++ src/components/AboutContent/AboutItem.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const AboutItem = ({ img, title, text, style, alt }) => {
   return (
-    <li className={`bg-purple flex flex-col items-center ${style}`}>
+    <li
+      className={`bg-purple flex flex-col items-center min-w-[280px] py-[5rem] px-8 ${style}`}
+    >
       <img className="mb-1" src={img} alt={alt} />
       <h2 className="text-yellow mb-2 text-2xl font-medium">{title}</h2>
       <p className="text-white-100 text-base text-center">{text}</p>
