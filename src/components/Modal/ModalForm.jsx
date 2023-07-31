f09a95e42a459b24da72472162dd552a15533096
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-31 13:44:44 -0300

    :sparkles: feat: add modalForm footer component

---
+++ src/components/Modal/ModalForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+
+const ModalForm = ({ isOpen }) => {
+  return (
+    <form
+      className={`flex lg:gap-4 gap-4 ${
+        isOpen ? "flex-col" : "hidden"
+      } lg:flex`}
+    >
+      <Input placeholder="Write your username..." />
+      <Input type="password" placeholder="Write your password..." />
+      <Button color="bg-green">Search</Button>
+    </form>
+  );
+};
+
+export default ModalForm;
