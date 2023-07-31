2a070dde5e1a60e0eed3f52818e25a107c24fc4f
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-31 13:24:57 -0300

    :recycle: refactor(routes): change name about component

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import FeaturedContent from "../components/FeaturedContent/FeaturedContent";
-import AboutContent from "../components/AboutContent/AboutContent";
+import AboutCards from "../components/AboutContent/AboutContent";
 import useMedia from '../hooks/useMedia'
+
 const About = () => {
   const screen = useMedia('(min-width:1535px)') 
   
   return (
-    <main className="grid 2xl:grid-cols-2 max-w-[1600px] mx-auto 2xl:justify-items-end items-center px-10">
-      <AboutContent />
+    <main className="container grid 2xl:grid-cols-2 2xl:justify-items-end items-center">
+      <AboutCards />
       {screen && <FeaturedContent />}
     </main>
   );
