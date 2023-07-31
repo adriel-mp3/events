d020ab869eacc45916d7e4dddfb0df98a1859123
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-31 13:31:55 -0300

    :recycle: refactor(components): refactor header into smaller components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,112 +1,43 @@
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
 import Button from "../Button/Button";
-import logo from "../../assets/images/logo.svg";
 import ModalLogin from "../Modal/ModalLogin";
-import open from "../../assets/images/open.svg";
-import close from "../../assets/images/close.svg";
 import useMedia from "../../hooks/useMedia";
 import useToggle from "../../hooks/useToggle";
+import Logo from "./Logo";
+import MenuHamburger from "./MenuHamburger";
+import NavMenu from "./NavMenu";
 
 const Header = () => {
-  const { pathname } = useLocation();
   const [modal, setModal] = useToggle(false);
   const [menu, setMenu] = useToggle(false);
   const mobile = useMedia("(max-width: 1024px)");
 
-  function handleClickGetTicket() {
+  function toggleGetTicketModal() {
     setModal();
     if (menu) {
       setMenu();
     }
   }
 
-  function handleClickMenu() {
+  function toggleMenuMobile() {
     if (mobile) {
       setMenu();
     }
   }
 
   return (
-    <div className="justify-center w-screen ">
-      <header className="flex max-w-[1600px] justify-between py-8 mx-auto mb-16 px-10">
-        <div className="flex items-center gap-4">
-          <img src={logo} alt="Events logo" />
-          <span className="flex items-center gap-4 text-2xl-bold text-white-100  before:content-[''] before:block before:w-[0.25rem] before:h-8 before:bg-white-100">
-            Events
-          </span>
-        </div>
-        <button className={"lg:hidden block"} onClick={handleClickMenu}>
-          <img src={menu ? close : open} alt="Menu icon" />
-        </button>
-        <nav
-          className={`lg:flex items-center lg:gap-6 ${
-            menu
-              ? "flex flex-col text-center gap-4 bg-purple-100 w-screen h-[100%] absolute z-20 left-0 top-0 pt-9"
-              : "hidden"
-          }`}
-        >
-          {menu && (
-            <div className="flex items-center gap-4 w-screen justify-between">
-              <div className="flex items-center gap-4 px-10">
-                <img src={logo} alt="Events logo" />
-                <span className="flex items-center gap-4 2xl-bold text-white-100 font-medium before:content-[''] before:block before:w-[0.25rem] before:h-8 before:bg-white-100">
-                  Menu
-                </span>
-              </div>
-              <button className="block pr-10" onClick={handleClickMenu}>
-                <img src={menu ? close : open} alt="Menu icon" />
-              </button>
-            </div>
-          )}
-          <ul
-            className="flex lg:flex-row flex-col gap-4 lg:items-center text-base-bold text-white-100 font-medium lg:gap-6"
-            onClick={handleClickMenu}
-          >
-            <li>
-              <NavLink
-                to="/"
-                end
-                className={`py-2   ${
-                  pathname === "/" ? "border-b-[3px] border-red" : ""
-                }`}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className={`py-2 ${
-                  pathname === "/about" ? "border-b-[3px] border-red" : ""
-                }`}
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/previous-events"
-                className={`py-2 ${
-                  pathname === "/previous-events"
-                    ? "border-b-[3px] border-red"
-                    : ""
-                }`}
-              >
-                Previous Events
-              </NavLink>
-            </li>
-          </ul>
-          <Button
-            style={`bg-red ${menu ? "justify-start align mt-10" : ""}`}
-            onClick={handleClickGetTicket}
-          >
+    <>
+      <header className="container justify-between py-8 mb-16">
+        <Logo isOpen={menu} />
+        <MenuHamburger onClick={toggleMenuMobile} isOpen={menu} />
+        <NavMenu isMenuOpen={menu} toggleMenu={toggleMenuMobile}>
+          <Button color={"bg-red"} onClick={toggleGetTicketModal}>
             Get a ticket
           </Button>
-        </nav>
+        </NavMenu>
       </header>
       <ModalLogin modal={modal} setModal={setModal} />
-    </div>
+    </>
   );
 };
 
