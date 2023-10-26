f01b049c686e1a4b09cc989e67f5fa9be9e073e6
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-26 16:48:53 -0300

    refactor: change label for children prop

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import logo from "../../assets/images/logo.svg";
 import { Link } from "react-router-dom";
-const Logo = ({ label }) => {
+
+export const Logo = ({ children }) => {
+  
   return (
     <Link to="/" className="flex items-center gap-4 z-20">
       <img src={logo} alt="Events logo" />
       <span className="flex items-center gap-4 text-2xl-bold text-white-100 logo-separator">
-        {label}
+        {children}
       </span>
     </Link>
   );
 };
 
-export default Logo;
