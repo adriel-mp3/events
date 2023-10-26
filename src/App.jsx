afe7b6ec61194c582b0c12d71577ab1f3b3decaf
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-26 16:50:42 -0300

    feat: import headerLinks data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import PreviousEvents from "./pages/PreviousEvents";
-import Header from "./layout/Header/Header";
+import Header from "./layout/Header/index";
 import Footer from "./layout/Footer/Footer";
+import { headerLinks } from "./utils/headerLinks";
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
       <BrowserRouter>
-        <Header />
+        <Header links={headerLinks} />
         <div className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
