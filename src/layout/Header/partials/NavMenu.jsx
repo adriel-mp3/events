5a38c02e1590188551819b6902421a3637693fbb
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-26 16:46:24 -0300

    feat: create NavMenu

---
+++ src/layout/Header/partials/NavMenu.jsx
@@ -0,0 +1,47 @@
+import { NavLink, useLocation } from "react-router-dom";
+
+import { Button } from "../../../components/Button/Button";
+import { ModalLogin } from "../../../components/Modal/ModalLogin";
+import useToggle from "../../../hooks/useToggle";
+
+export const NavMenu = ({ isMenuOpen, toggleMenu, links }) => {
+  const [modal, setModal] = useToggle(false);
+  const { pathname } = useLocation();
+
+  const activeLinkStyle = "border-b-[3px] border-red"
+
+  return (
+    <>
+      <nav
+        className={`lg:flex items-center lg:gap-6 ${isMenuOpen
+          ? "flex flex-col text-center gap-4 bg-purple-100 w-screen h-[100%] absolute z-10 left-0 top-0 pt-[160px]"
+          : "hidden"
+          }`}
+      >
+        <ul
+          className="flex flex-col gap-4 lg:gap-6 lg:flex-row lg:items-center text-base-bold text-white-100"
+          onClick={toggleMenu}
+        >
+          {links.map((link) => (
+            <li key={link.label}>
+              <NavLink
+                to={link.to}
+                end={link.end}
+                className={`py-1 ${pathname === link.to ? activeLinkStyle : ""}`}
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <Button color={"bg-red"} onClick={setModal} >
+          Get a ticket
+        </Button>
+      </nav>
+      <ModalLogin modal={modal} setModal={setModal} />
+    </>
+
+  );
+};
+
+
