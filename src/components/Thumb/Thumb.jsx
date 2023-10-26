e8f60a03533f1a84a71f426bf1d5ec4c637f4fbc
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-26 17:16:01 -0300

    refactor: add named export

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import thumb from "../../assets/images/thumb.jpg";
-const Thumb = () => {
+
+export const Thumb = ({ thumb }) => {
   return (
     <div className="relative after:content-[''] after:bg-play after:w-[55px] after:h-[66px] after:absolute flex items-center justify-center lg:mb-0 mb-10">
       <img
@@ -11,4 +11,4 @@ const Thumb = () => {
   );
 };
 
-export default Thumb;
+
