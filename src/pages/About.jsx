cd2ec45a15abab2409a6f9cc0cadaa6936553fa0
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-26 17:32:43 -0300

    refactor: add named exports

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import FeaturedContent from "../components/FeaturedContent/FeaturedContent";
-import AboutCards from "../components/AboutContent/AboutContent";
-import useMedia from '../hooks/useMedia'
+import useMedia from '../hooks/useMedia';
+
+import { FeaturedContent } from "../components/FeaturedContent/FeaturedContent";
+import { AboutCards } from '../components/AboutCards';
+
+import { aboutCardsData } from '../utils/aboutCardsData';
 
 const About = () => {
-  const screen = useMedia('(min-width:1535px)') 
-  
+  const screen = useMedia('(min-width:1535px)')
+
   return (
     <main className="container grid 2xl:grid-cols-2 2xl:justify-items-end items-center">
-      <AboutCards />
+      <AboutCards aboutCardsData={aboutCardsData} />
       {screen && <FeaturedContent />}
     </main>
   );
